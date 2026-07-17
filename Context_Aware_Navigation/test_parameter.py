d053e65170e75e89d7269f8e0db24bd8ae5374ed
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2026-07-17 13:28:43 +0530

    Add Context_Aware_Navigation reproduction setup

---
+++ Context_Aware_Navigation/test_parameter.py
@@ -0,0 +1,16 @@
+FOLDER_NAME = 'backup'
+model_path = f'model/{FOLDER_NAME}'
+gifs_path = f'results/{FOLDER_NAME}/gifs'
+trajectory_path = f'results/trajectory'
+length_path = f'results/length'
+INPUT_DIM = 7
+EMBEDDING_DIM = 128
+K_SIZE = 20  # the number of neighbors
+USE_GPU = False  # do you want to use GPUS?
+NUM_GPU = 0 # the number of GPUs
+NUM_META_AGENT = 1  # the number of processes
+NUM_TEST = 20
+NUM_RUN = 1
+SAVE_GIFS = False  # do you want to save GIFs
+SAVE_TRAJECTORY = False  # do you want to save per-step metrics
+SAVE_LENGTH = False  # do you want to save per-episode metrics
